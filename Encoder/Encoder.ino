cff57d6a77c648a5a8511c48e55157987012ce66
Henning Eggertz <henning.eggertz@gmail.com>
2020-11-26 20:57:48 +0100

    This is the firsst try!

---
+++ Encoder/Encoder.ino
@@ -0,0 +1,31 @@
+const int clkPin = 2;
+const int DT = 4;
+const int encoderButt = 3;
+
+void setup() {
+pinMode(clkPin, INPUT_PULLUP);
+pinMode(DT, INPUT_PULLUP);
+pinMode(encoderButt, INPUT_PULLUP);
+attachInterrupt(0, encoder, FALLING);
+attachInterrupt(1, encoderButton, FALLING);
+
+Serial.begin(9600);
+}
+
+void loop() {
+  Serial.print("\n");
+  delay(1000);
+}
+
+void encoder(){
+  if(digitalRead(DT) == LOW){
+    Serial.println("Clockvise");
+  }
+  else{
+    Serial.println("Anticlockvise");
+  }
+}
+
+void encoderButton(){
+  Serial.println("Clickde");
+}
