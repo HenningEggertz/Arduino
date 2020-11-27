5d3ee8b812ac7f638cd3d1416918916f6f2d1a92
Henning Eggertz <henning.eggertz@gmail.com>
2020-11-27 14:30:18 +0100

    Kommentar för ISR

--- Encoder/Encoder.ino
+++ Encoder/Encoder.ino
@@ -5,11 +5,12 @@ const int DT = 4;
 const int encoderButt = 3;
 
 void setup() {
-
 DDRD = ((0 << clkPin) | (0 << DT) | (0 << encoderButt)); // This pins are now inputs
 PORTD = ((1 << clkPin) | (1 << DT) | (1 << encoderButt)); //Sets the pins internal pullup resistors
 
-
+/*GIMSK = (1 << PCIE);
+sei();
+*/
 attachInterrupt(0, encoder, FALLING);
 attachInterrupt(1, encoderButton, FALLING);
 
